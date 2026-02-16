7e11ce39d75c56c12257cc9a9e89d15359ba0da9
ixynrs <ixynrs@gmail.com>
2026-02-17 02:57:05 +0800

    Updated financial report tests to improve clarity and included the SRE for testing

--- tests/Feature/FinancialReportGenerationTest.php
+++ tests/Feature/FinancialReportGenerationTest.php
@@ -2,20 +2,41 @@
 
 use function Pest\Laravel\get;
 
-const SAMPLE_DOWNLOAD_NAME = 'Q1-2025-MAR-JUN-AMD.pdf';
+const AMD_DOWNLOAD_NAME = 'Q1-2025-MAR-JUN-AMD.pdf';
+const SRE_DOWNLOAD_NAME = 'SRE-Manila-City-Q1-2025.pdf';
 
 test('report home page is accessible', function () {
-    $response = get(route('reports.home'));
+    $response = get(route('home'));
 
-    $response
-        ->assertSuccessful()
-        ->assertSee('AMD Q1 Report Downloader')
-        ->assertSee('public/financial-reports/amd-q1.json');
+    $response->assertStatus(200);
 });
 
-test('download endpoint returns the amd q1 pdf file', function () {
+test('amd q1 preview endpoint returns the amd q1 pdf file', function () {
+    $response = get(route('reports.preview'));
+
+    $response
+        ->assertHeaderContains('content-disposition', 'inline')
+        ->assertHeaderContains('content-disposition', 'filename=');
+});
+
+test('amd q1 download endpoint returns the amd q1 pdf file', function () {
     $response = get(route('reports.download'));
 
     $response
-        ->assertDownload(SAMPLE_DOWNLOAD_NAME);
+        ->assertDownload(AMD_DOWNLOAD_NAME);
+});
+
+test('sre preview endpoint returns the sre pdf file', function () {
+    $response = get(route('sre.preview'));
+
+    $response
+        ->assertHeaderContains('content-disposition', 'inline')
+        ->assertHeaderContains('content-disposition', 'filename=');
+});
+
+test('sre download endpoint returns the sre pdf file', function () {
+    $response = get(route('sre.download'));
+
+    $response
+        ->assertDownload(SRE_DOWNLOAD_NAME);
 });
